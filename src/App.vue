cebea71432049508242436153fb2971b852df8b0
ovuoba sampson <ovuobasampson@gmail.com>
2019-02-16 10:00:48 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <TodoList v-bind:todos="todos" />
+    <!-- <CreateTodo v-on:add-todo="addTodo" /> -->
+    <CreateTodo v-on:create-todo="addTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo.vue";
+
+export default {
+  name: "app",
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: "First Todo",
+          project: "Project 1",
+          done: false
+        },
+        {
+          title: "Second Todo",
+          project: "Project 2",
+          done: true
+        },
+        {
+          title: "Third Todo",
+          project: "Project 3",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
